07b8b412012d724402f62ac6e6c2e2b036f23032
martinstacho <martin.stacho@gmail.com>
2026-04-27 14:15:02 +0200

    feat(i18n): auto-detect browser language and redirect to matching locale

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,4 +3,6 @@ import { defineRouting } from 'next-intl/routing'
 export const routing = defineRouting({
   locales: ['sk', 'en', 'uk', 'hu'],
   defaultLocale: 'sk',
+  localeDetection: true,  // redirect based on Accept-Language header (cookie NEXT_LOCALE takes priority)
+  localePrefix: 'always', // all URLs carry /sk, /en, /uk, /hu prefix
 })
